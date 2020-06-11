36fbca3a160950228e6831f1a02e223dd1f2b608
reinterpretcat <ilya.builuk@gmail.com>
2020-06-11 22:23:37 +0200

    Add lifecycle rule to s3 bucket

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "solver_data" {
   bucket = var.solver_data_bucket
   acl = "private"
 
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = var.solver_data_bucket_expiration
+    }
+  }
+
   tags = {
     description = "A bucket for storing problems and solutions used by VRP solver"
     environment = var.env_suffix_name
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,10 @@ variable "solver_data_bucket" {
   type = string
   default = "vrp-solver-data-dev"
 }
+
+variable "solver_data_bucket_expiration" {
+  description = "Amount of days after which objects are deleted in solver's bucket"
+
+  type = number
+  default = 3
+}
