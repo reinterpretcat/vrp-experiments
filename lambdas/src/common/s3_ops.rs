7b9ec41fa75767190e7c1f1bcc0dfa53472bec47
reinterpretcat <ilya.builuk@gmail.com>
2020-06-16 00:27:11 +0200

    Improve download from s3

--- lambdas/src/common/s3_ops.rs
+++ lambdas/src/common/s3_ops.rs
@@ -1,8 +1,9 @@
 use crate::common::AppError;
+use bytes::BytesMut;
+use futures::TryStreamExt;
 use rusoto_core::Region;
 use rusoto_s3::{GetObjectRequest, PutObjectRequest, S3Client, S3};
 use std::error::Error;
-use std::io::Read;
 
 /// Uploads string data to s3 bucket using parameters specified.
 pub async fn upload_to_s3(
@@ -35,37 +36,37 @@ pub async fn download_from_s3(
     bucket: String,
     key: String,
 ) -> Result<String, AppError> {
-    let mut object = S3Client::new(region)
+    let download_error = |err: String| AppError {
+        message: "cannot download from s3".to_string(),
+        details: format!("bucket: '{}', key: '{}', info: '{}'", bucket, key, err),
+    };
+
+    let object = S3Client::new(region)
         .get_object(GetObjectRequest {
             bucket: bucket.to_string(),
             key: key.clone(),
             ..Default::default()
         })
         .await
-        .map_err(|err| AppError {
-            message: format!("cannot download from s3: '{}/{}'", bucket, key),
-            details: err
-                .source()
-                .map(|err_src| format!("{}", err_src))
-                .unwrap_or_else(|| "unknown".to_string()),
+        .map_err(|err| {
+            download_error(
+                err.source()
+                    .map(|err_src| format!("{}", err_src))
+                    .unwrap_or_else(|| "unknown".to_string()),
+            )
         })?;
 
-    let mut buffer = Vec::new();
-    object
+    let body = object
         .body
-        .take()
-        .ok_or_else(|| "cannot get body".to_string())
-        .and_then(|body| {
-            body.into_blocking_read()
-                .read_to_end(&mut buffer)
-                .map_err(|err| format!("cannot read body: '{}'", err))
-        })
-        .and_then(|_| {
-            String::from_utf8(buffer)
-                .map_err(|err| format!("cannot convert utf8 string: '{}'", err))
-        })
-        .map_err(|details| AppError {
-            message: "cannot download s3 object".to_string(),
-            details: format!("s3 object key '{}', error: {}", key, details),
-        })
+        .ok_or_else(|| download_error("body is empty".to_string()))
+        .map(|stream| stream.map_ok(|b| BytesMut::from(&b[..])).try_concat())?
+        .await
+        .map_err(|err| download_error(format!("cannot get body: '{}'", err)));
+
+    let content = body.and_then(|body| {
+        String::from_utf8(body.to_vec())
+            .map_err(|err| download_error(format!("cannot convert to utf8 string: '{}'", err)))
+    });
+
+    content
 }
