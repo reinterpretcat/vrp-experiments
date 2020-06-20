71098fd7abf4edd05b48009429f31724f697456a
reinterpretcat <ilya.builuk@gmail.com>
2020-06-20 10:05:44 +0200

    Add terraform configuration for solution polling

--- terraform/api.tf
+++ terraform/api.tf
@@ -6,13 +6,26 @@ resource "aws_api_gateway_rest_api" "vrp_solver_api" {
 resource "aws_api_gateway_resource" "problem_api_resource" {
   rest_api_id = aws_api_gateway_rest_api.vrp_solver_api.id
   parent_id = aws_api_gateway_rest_api.vrp_solver_api.root_resource_id
-  path_part = "{problem+}"
+  path_part = "problem"
+}
+
+resource "aws_api_gateway_resource" "solution_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.vrp_solver_api.id
+  parent_id = aws_api_gateway_rest_api.vrp_solver_api.root_resource_id
+  path_part = "solution"
 }
 
 resource "aws_api_gateway_method" "problem_api_resource_method" {
   rest_api_id = aws_api_gateway_rest_api.vrp_solver_api.id
   resource_id = aws_api_gateway_resource.problem_api_resource.id
   http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "solution_api_resource_method" {
+  rest_api_id = aws_api_gateway_rest_api.vrp_solver_api.id
+  resource_id = aws_api_gateway_resource.solution_api_resource.id
+  http_method = "GET"
   authorization = "NONE"
 }
 
@@ -26,15 +39,22 @@ resource "aws_api_gateway_integration" "submit_problem_api_gateway_integration" 
   uri = aws_lambda_function.submit_problem_function.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "submit_problem_api_deployment" {
+resource "aws_api_gateway_integration" "poll_solution_api_gateway_integration" {
+  rest_api_id = aws_api_gateway_rest_api.vrp_solver_api.id
+  resource_id = aws_api_gateway_method.solution_api_resource_method.resource_id
+  http_method = aws_api_gateway_method.solution_api_resource_method.http_method
+
+  integration_http_method = "GET"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.poll_solution_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "vrp_solver_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.submit_problem_api_gateway_integration,
+    aws_api_gateway_integration.poll_solution_api_gateway_integration,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.vrp_solver_api.id
-  stage_name  = "test"
-}
-
-output "base_url" {
-  value = aws_api_gateway_deployment.submit_problem_api_deployment.invoke_url
+  stage_name = "test"
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,6 +6,10 @@ data "local_file" "trigger_solver_zip" {
   filename = "../artifacts/trigger_solver.zip"
 }
 
+data "local_file" "poll_solution_zip" {
+  filename = "../artifacts/poll_solution.zip"
+}
+
 resource "aws_lambda_function" "submit_problem_function" {
   description = "A lambda function which receives VRP definition and stores in s3 bucket"
   filename = data.local_file.submit_problem_zip.filename
@@ -42,10 +46,36 @@ resource "aws_lambda_function" "trigger_solver_function" {
   }
 }
 
+resource "aws_lambda_function" "poll_solution_function" {
+  description = "A lambda function which polls VRP solution"
+  filename = data.local_file.poll_solution_zip.filename
+  source_code_hash = filebase64sha256(data.local_file.poll_solution_zip.filename)
+
+  function_name = "poll_solution"
+  handler = "ignored"
+  role = aws_iam_role.vrp_solver_lambda_role.arn
+  runtime = "provided"
+
+  environment {
+    variables = {
+      SOLVER_BUCKET_NAME = aws_s3_bucket.vrp_solver_data.bucket
+    }
+  }
+}
+
 resource "aws_lambda_permission" "vrp_solver_allow_submit_from_gateway" {
   statement_id = "AllowAPIGatewayInvoke"
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.submit_problem_function.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.vrp_solver_api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "vrp_solver_allow_poll_from_gateway" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.poll_solution_function.function_name
   principal = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.vrp_solver_api.execution_arn}/*/*"
