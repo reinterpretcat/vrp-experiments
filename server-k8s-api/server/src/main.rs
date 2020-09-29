e7d975e163208d5bb1f8ed0da29f938a3647636c
reinterpretcat <ilya.builuk@gmail.com>
2020-09-29 21:56:47 +0200

    Pass port value from outside

--- server-k8s-api/server/src/main.rs
+++ server-k8s-api/server/src/main.rs
@@ -7,12 +7,17 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+    let port = std::env::var("PORT")
+        .map_err(|err| err.to_string())
+        .and_then(|value| value.parse::<i32>().map_err(|err| err.to_string()))
+        .unwrap_or_else(|err| panic!("cannot get port value from PORT: {}", err));
+
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .service(web::resource("/problems").route(web::post().to(problem::solution)))
     })
-    .bind("127.0.0.1:8000")?
+    .bind(format!("127.0.0.1:{}", port).as_str())?
     .run()
     .await
 }
